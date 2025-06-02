e4652c6830cc3f68611ec08861e18529f9f6a160
akshayitzme <akshaypooloth1@gmail.com>
2025-06-02 12:35:09 +0530

    feat: added ms-clarity

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import router from "./router";
+import Clarity from "@microsoft/clarity";
 
-createApp(App).use(router).mount('#app')
+const projectId = "rt2qldwzil";
+Clarity.init(projectId);
+
+createApp(App).use(router).mount("#app");
