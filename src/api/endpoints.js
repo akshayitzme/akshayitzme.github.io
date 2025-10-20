31c070a335af7704b2e20b7246e96c1911173cc4
akshayitzme <akshaypooloth1@gmail.com>
2025-10-20 20:45:17 +0530

    fix: update endpoint to use https

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,6 +1,6 @@
 const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL ||
-  "http://akshayitzme.duckdns.org:8000/api/v1/watched";
+  "https://akshayitzme.duckdns.org/api/v1/watched";
 
 export const endpoints = {
   watched: `${API_BASE_URL}/watched`,
